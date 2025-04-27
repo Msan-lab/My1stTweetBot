16fdba1003d69373c3f5457993d89fbc69a1ec1c
M-san <166983319+Msan-lab@users.noreply.github.com>
2025-04-28 01:05:20 +0900

    Update My1stTweetBot.py

--- My1stTweetBot.py
+++ My1stTweetBot.py
@@ -9,12 +9,13 @@ API_SECRET = os.environ['TWITTER_API_SECRET']
 ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
-auth = tweepy.OAuth1UserHandler(
-    API_KEY, API_SECRET,
-    ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+client = tweepy.Client(
+    consumer_key=API_KEY,
+    consumer_secret=API_SECRET,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_TOKEN_SECRET
 )
-api = tweepy.API(auth)
 tweet_text = "はじめてのTweet Botテスト"
 
 # ツイート実行
-api.update_status(status=tweet_text)
+response = client.create_tweet(text=tweet_text)
